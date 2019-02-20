e966cf0ea00b8f4ea11bbee9ccc2b5a8f7d734e2
陈泽 <chenze_fdu@163.com>
2019-02-20 15:10:51 +0800

    modify entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,10 @@
 from scrapy.cmdline import execute
 
+# 基于用户id搜索： 第一位：type = 1 第二位：用户名  第三位：用户id 第四位：页数 第五位：是否要图片
+# 例子：   1,重庆发布,1988438334,3,True
+# 基于关键词搜索：  第一位：type = 2   第二位：关键词    第三位：1-综合 60-热门 61-实时  第四位：页数 第五位：是否要图片
+# 例子    2,天盛长歌,60,3,True
+
 # execute(["scrapy", "crawl", "weibo", "-a",
 #          "line=2,天盛长歌,60,2,False@_@2,重庆,61,2,False"])
 
